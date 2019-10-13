426ac3a59abd8240922ee3f350a655f2acd00e2f
akGmit <g00328529@gmit.ie>
2019-10-13 03:56:30 +0100

    C to read files, arrays created from mnist data, **rough workd

--- rough_work/readFiles.c
+++ rough_work/readFiles.c
@@ -11,6 +11,7 @@ int main()
   uint8_t *buffer;
   
   pFile = fopen("t10k-images.idx3-ubyte", "rb");
+  //pFile = fopen("t10k-labels.idx1-ubyte", "rb");
   // Get file size
   fseek(pFile, 0, SEEK_END);
   lSize = ftell(pFile);
@@ -25,9 +26,35 @@ int main()
   // convert first 8 bytes to magic number and items count
   // big endian
   uint32_t magicNumber = (buffer[0] << 24) + (buffer[1] << 16) + (buffer[2] << 8) + buffer[3];
-  uint32_t numberOfItems = (buffer[4] << 24) + (buffer[5] << 16) + (buffer[6] << 8) + buffer[7];
+  uint32_t num_images  = (buffer[4] << 24) + (buffer[5] << 16) + (buffer[6] << 8) + buffer[7];
   
-  printf("%d  %d", magicNumber, numberOfItems);
+  uint8_t img_arr[num_images][28][28];
+  uint8_t img_labels[num_images];
+  
+  if(magicNumber == 2049){
+    int pos = 8;
+    for(int i = 0; i < num_images; i++){
+        img_labels[pos] = buffer[pos];     
+        printf("%d ", img_labels[pos]);
+        pos++;
+    }
+  }
+  
+  if(magicNumber == 2051){
+    int pos = 16;
+    for(int i = 0;i < num_images; i++){
+      for(int j = 0; j < 28; j++){
+        for(int k = 0; k < 28; k++){
+          img_arr[i][j][k] = buffer[pos];
+          printf("%s ", img_arr[i][j][k] > 128 ? "0" : "~");
+          pos++;
+        }
+        printf("\n");
+      }
+    }
+  }
+
+
 
   fclose(pFile);
   free(buffer);
